d3ed9455dcda0003860f22c9060dfdd87b510d39
FranchescoSoto <u201821078@upc.edu.pe>
2022-06-20 16:50:46 -0500

    se añade boostrarp al proyecto, se añade una nav-bar

---
+++ src/components/navbar.js
@@ -0,0 +1,33 @@
+import './navbar.css';
+import 'bootstrap/dist/css/bootstrap.css';
+function Navbar() {
+  return (
+    <header>
+       <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="assets/imgdiente.png" alt="" width="30" height="24" class="d-inline-block align-text-top"/>
+                        Dental Soto
+                    </a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                   
+                    <ul class="navbar-nav">
+                   
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Servicios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Nosotros</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+  );
+}
+
+export default Navbar;
